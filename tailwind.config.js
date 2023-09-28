5b49df1b57ed43222dff549571705692b9584988
Ayusht777 <talesarayush@gmail.com>
2023-09-28 12:59:59 +0530

    02

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9333EA",
+        secondary: "#ff7e33",
+        info: "#0C63E7",
+      },
+    },
+  },
+  plugins: [],
+};
