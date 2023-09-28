0531c04d054003f376c7f36dc1d3ffa44541e2ab
unique011 <harshitsharma.2021cs@technonjr.org>
2023-09-28 22:10:13 +0530

    Navigation set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Clients, avtaarsImg } from "./Data/Data";
 import HeroSection from "./components/HeroSection";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 function App() {
   console.log(avtaarsImg);
   console.log(Clients);
   return (
     <div className=" box-border w-full h-full flex bg-bg justify-center items-center flex-col">
+      <NavigationBar />
       <HeroSection />
     </div>
   );
--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -4,7 +4,7 @@ const NavigationBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className=" Nagation w-[80%] h-16 m-auto py-4 flex justify-between align-middle">
+    <div className=" Nagation w-[80%] h-20 m-auto py-6 flex justify-between align-middle">
       <div className="text-textH text-2xl font-extrabold flex place-items-center gap-3">
         <div className="flex gap-1">
           <div className="h-5 w-5 rounded-[100%] bg-white"></div>
@@ -12,48 +12,51 @@ const NavigationBar = () => {
         </div>
         {NavCompany.title}
       </div>
-      {!isOpen ? (
-        <button
-          className="flex md:hidden gap-2 flex-col text-lg align-middle justify-center text-textH "
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-        >
-          <div className="w-10 h-1 bg-gray-400"></div>
-          <div className="w-10 h-1 bg-gray-400"></div>
-        </button>
-      ) : (
+
+      <button
+        className="flex md:hidden gap-2 flex-col text-lg align-middle justify-center text-textH "
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
         <div
-          className="font-semibold relative -top-3 text-gray-400 text-5xl "
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-        >
-          x
-        </div>
-      )}
+          className="w-10 h-1 bg-gray-400 "
+          style={
+            isOpen
+              ? {
+                  transform: "rotate(-45deg)",
+                }
+              : {}
+          }
+        ></div>
+        <div
+          className="w-10 h-1 bg-gray-400 relative bottom-3 transition-all "
+          style={isOpen ? { transform: "rotate(45deg)" } : { bottom: "0" }}
+        ></div>
+      </button>
+
       {isOpen && (
-        <div className="absolute h-screen w-screen right-0 top-16 bg-bg">
-          <div className="flex justify-start flex-col  gap-4">
+        <div className="absolute h-screen w-screen right-0 top-16 bg-bg ">
+          <div className="flex justify-start align-middle flex-col bg-[#6d6b75aa] w-[90%] rounded-lg m-auto my-7 gap-5 p-7">
             {NavigationData.map((value, key) => {
               return (
-                <div key={key} className="m-auto h-auto">
+                <div key={key} className="m-0 h-auto ">
                   <a
                     href={value.link}
-                    className="text-textP hover:text-textH text-lg"
+                    className="text-[#b8b2b2] hover:text-textH text-lg w-36"
                   >
                     {value.item}
                   </a>
                 </div>
               );
             })}
-            <button className="px-4 py-0  text-textH text-base rounded-3xl  bg-btn hover:scale-105">
+            <button className="px-4 py-1 w-36  text-textH text-base rounded-3xl  bg-btn hover:scale-105">
               Get Started
             </button>
           </div>
         </div>
       )}
-      <div className="hidden md:flex align-middle justify-around gap-4">
+      <div className="hidden md:flex align-middle justify-around gap-7">
         {NavigationData.map((value, key) => {
           return (
             <div key={key} className="m-auto h-auto">
