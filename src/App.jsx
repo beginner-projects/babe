0502c33f1c0f832673219dda6ee21dc194ebce7e
Ayusht777 <talesarayush@gmail.com>
2023-09-28 19:58:19 +0530

    08

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import HeroSection from "./components/HeroSection";
-import Data from "./Data/Data"
+
 function App() {
   return (
     <div className=" box-border w-full h-full flex bg-bg justify-center items-center flex-col">
       <HeroSection />
-      {console.log(Data)}
+      
     </div>
   );
 }
