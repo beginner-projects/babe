6156dd055d39764face8e0995ceebdf0e0dfca74
Ayusht777 <talesarayush@gmail.com>
2023-09-28 23:41:46 +0530

    updated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import HeroSection from "./components/HeroSection";
-// import Contacts from "./components/Contacts";
+import Contacts from "./components/Contacts";
+import LatestArticleSection from "./components/LatestArticleSection";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className=" box-border w-full h-full flex bg-bg justify-center items-center flex-col">
       <NavigationBar />
       <HeroSection />
-      {/* <Contacts /> */}
+      <Contacts />
+      <LatestArticleSection />
     </div>
   );
 }
