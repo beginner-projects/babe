16cb9ec3c7aeab9e929922109bebbd8b40a73b6e
Ayusht777 <talesarayush@gmail.com>
2023-09-28 16:52:37 +0530

    05

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className=" box-border w-full h-full m-0 p-0">a</div>
+      <div className=" box-border w-full h-full m-0 p-0 bg-bg"></div>
     </>
   );
 }
