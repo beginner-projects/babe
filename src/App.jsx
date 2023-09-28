a8df00bf7bc8aa45f2b2a21d4921d9c128698ccf
unique011 <harshitsharma.2021cs@technonjr.org>
2023-09-28 19:59:48 +0530

    harshit Navigatoin

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <div className="text-5xl text-primary">a</div>
-=======
       <div className=" box-border w-full h-full m-0 p-0 bg-bg"></div>
->>>>>>> 16cb9ec3c7aeab9e929922109bebbd8b40a73b6e
     </>
   );
 }
