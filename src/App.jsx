663181e3a5674a1751241b323ae09bb113259ed2
divyanshu_sahu <divyanshusahucoc@gmail.com>
2023-09-28 18:55:11 +0530

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import LatestArticleSection from "./components/LatestArticleSection";
 
 function App() {
   return (
     <>
-      <div className=" box-border w-full h-full m-0 p-0 bg-bg"></div>
+      <div className=" box-border w-full h-full m-0 p-0 bg-bg">
+        <LatestArticleSection/>
+      </div>
     </>
   );
 }
