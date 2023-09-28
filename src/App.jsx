f2961c74d42e698a3e8121985cca17128ea76896
Ayusht777 <talesarayush@gmail.com>
2023-09-28 19:14:08 +0530

    06

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import HeroSection from "./components/HeroSection.jsx"
 function App() {
   return (
     <>
-      <div className=" box-border w-full h-full m-0 p-0 bg-bg"></div>
+      <div className="box-border w-full h-full  bg-bg flex justify-center items-center">
+      <HeroSection/>
+      </div>
     </>
   );
 }
